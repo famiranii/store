91d9417225a3881d2abdbb0b94d6613ff97033e0
farhad40 <you@example.com>
2023-10-23 18:57:50 +0330

    for gh-pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,10 @@
 import Cart from "./pages/Cart/Cart";
-import AdminPanel from "./pages/AdminPanel/AdminPanel";
 import Product from "./pages/Products/Products";
 import Regester from "./pages/Regester/Regester";
 import Login from "./pages/Login/Login";
 
 const routes = [
   { path: "/", element: <Product /> },
-  { path: "/panel", element: <AdminPanel /> },
   { path: "/cart", element: <Cart /> },
   { path: "/regester", element: <Regester /> },
   { path: "/login", element: <Login /> },
